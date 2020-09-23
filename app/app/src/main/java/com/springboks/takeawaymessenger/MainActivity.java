f281444f9241719dc9d3b6e2816f722bf4a3eafb
semnijenhuis <470066@student.saxion.nl>
2020-09-23 16:28:24 +0200

    update 4

--- app/app/src/main/java/com/springboks/takeawaymessenger/MainActivity.java
+++ app/app/src/main/java/com/springboks/takeawaymessenger/MainActivity.java
@@ -3,12 +3,19 @@ package com.springboks.takeawaymessenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        imageView = findViewById(R.id.imageView);
+
         setContentView(R.layout.activity_main);
+
+
     }
 }
