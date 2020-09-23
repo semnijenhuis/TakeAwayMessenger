10d766644e5395ff35c48610e6005d1f0528d490
Stephen Nedd <484629@student.saxion.nl>
2020-09-23 10:29:29 -0400

    added activities folder

---
+++ app/app/src/main/java/com/springboks/takeawaymessenger/Activities/MainActivity.java
@@ -0,0 +1,32 @@
+package com.springboks.takeawaymessenger.Activities;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.springboks.takeawaymessenger.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+        //getSupportActionBar().setElevation(0);
+        View view = getSupportActionBar().getCustomView();
+        TextView name = view.findViewById(R.id.name);
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You have clicked tittle", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
