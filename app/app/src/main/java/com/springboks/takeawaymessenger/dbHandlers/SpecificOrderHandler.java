954edc0b8eb902d026f4172808058bf016c3337c
Max Sterken <485736@student.saxion.nl>
2020-10-23 11:39:12 +0200

    did some specificOrdeHandler stuff

--- app/app/src/main/java/com/springboks/takeawaymessenger/dbHandlers/SpecificOrderHandler.java
+++ app/app/src/main/java/com/springboks/takeawaymessenger/dbHandlers/SpecificOrderHandler.java
@@ -16,53 +16,51 @@ import com.springboks.takeawaymessenger.model.Order;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class SpecificOrderHandler {
-    private FirebaseFirestore db;
-    private DatabaseReference rootRef;
-
-    public interface onSpecificOrderReceivedListener{
-        public void displayOrder(Order order);
-    }
-    private SpecificOrderHandler.onSpecificOrderReceivedListener listener;
-
-    public SpecificOrderHandler(int OrderId) {
-        db = FirebaseFirestore.getInstance();
-        rootRef = FirebaseDatabase.getInstance().getReference();
-        this.listener= null;
-        getOrder(OrderId);
-    }
-
-    public void setOnSpecificOrderReceivedListener(SpecificOrderHandler.onSpecificOrderReceivedListener listener){
-        this.listener = listener;
-    }
-
-
-
-
-    public void getOrder(int orderId) {
-        CollectionReference ordersRef = db.collection("orders");
-
-        ordersRef.whereArrayContains("orderId",orderId).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    Order order = null;
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        if (!document.getData().isEmpty()) {
-                            order =document.toObject(Order.class);
-
-                            }
-                        }
-
-
-                    if(order != null){
-                        listener.displayOrder(order);
-                    }
-                } else {
-                    Log.d("DBHandlerG", "Error getting documents: ", task.getException());
-                }
-            }
-        });
-    }
-}
+//public class SpecificOrderHandler {
+//    private FirebaseFirestore db;
+//    private DatabaseReference rootRef;
+//
+//    public interface onSpecificOrderReceivedListener{
+//        public void displayOrder(Order order);
+//    }
+//    private SpecificOrderHandler.onSpecificOrderReceivedListener listener;
+//
+//    public SpecificOrderHandler(int OrderId) {
+//        db = FirebaseFirestore.getInstance();
+//        rootRef = FirebaseDatabase.getInstance().getReference();
+//        this.listener= null;
+//        getOrderByOrderNumber(OrderId);
+//    }
+//
+//    public void setOnSpecificOrderReceivedListener(SpecificOrderHandler.onSpecificOrderReceivedListener listener){
+//        this.listener = listener;
+//    }
+//
+//
+//
+//
+//    public Map<String, Object> getOrderByOrderNumber(final int orderNumberLoggedIn) {
+//        Log.i("luc", "im at get order by order number");
+//
+//        CollectionReference ordersRef = db.collection("orders");
+//        ordersRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                if (task.isSuccessful()) {
+//
+//                    for (QueryDocumentSnapshot document : task.getResult()) {
+//                        if (!document.getData().isEmpty()) {
+//                            int orderId = Integer.parseInt(document.getData().get("orderId").toString());
+//                            if (orderId == orderNumberLoggedIn){
+//                                orderData.putAll(document.getData());
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        });
+//        listener.displayOrder(orderData);
+//    }
+//}
